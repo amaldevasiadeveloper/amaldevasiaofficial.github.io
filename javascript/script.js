400907af73774b4a2f92f84dfe2dd5179523db6d
amaldevasiadeveloper <amaltitans2@gmail.com>
2020-08-19 22:01:03 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $("#submit-form").validate({
+        rules:{
+                name: "required",
+                email: "required",
+                phone: "required",
+                subject: "required",
+                message: "required",
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                phone: {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 10,
+                    number: true
+                },
+                subject: {
+                    required: true
+                },
+                message: {
+                    required: true
+                },
+
+            },
+            messages: {
+                name: "Please provide a valid name.",
+                email: {
+                    required: "Please enter your email",
+                    minlength: "Please enter a valid email address"
+                },
+                phone: {
+                    required: "Please provide a phone number",
+                    minlength: "Phone number must be min 10 characters long",
+                    maxlength: "Phone number must not be more than 10 characters long"
+                },
+                subject: "Please enter subject",
+                message: "Please enter your message"
+            },
+            submitHandler: function (form) {
+                form.submit();
+            }
+         })
+
+    })
